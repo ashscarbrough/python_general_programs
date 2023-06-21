3d0c981ad487a00e3537662676389f9e8aae4798
Ash Scarbrough <ash.scarbrough@gmail.com>
2023-06-21 11:56:00 -0400

    update comments

--- 00-commonquestions-examples/depth_first_search.py
+++ 00-commonquestions-examples/depth_first_search.py
@@ -1,62 +1,65 @@
-# Python3 program to print DFS traversal
-# from a given  graph
+'''
+Python program to print DFS traversal from a given  graph
+Time complexity: O(Vertices + Edges) : O(N)
+'''
+
 from collections import defaultdict
 
-
-# This class represents a directed graph using
-# adjacency list representation
 class Graph:
+    '''
+    This class creates directed graph using adjacency list representation
+    '''
 
-    # Constructor
     def __init__(self):
+        self.graph = defaultdict(list)  # Default dictionary to store graph
 
-        # Default dictionary to store graph
-        self.graph = defaultdict(list)
-
-
-    # Function to add an edge to graph
-    def addEdge(self, u, v):
-        self.graph[u].append(v)
+    def add_edge(self, vertex_item, edge):
+        '''
+        Function to add an edge to graph
+        '''
+        self.graph[vertex_item].append(edge)
 
 
     # A function used by DFS
-    def DFSUtil(self, v, visited):
+    def depth_first_search_util(self, vertex, visited):
+        '''
+        A function used by depth first search to account for visited graph neighbors
+        '''
 
-        # Mark the current node as visited
-        # and print it
-        visited.add(v)
-        print(v, end=' ')
+        # Mark the current node as visited and print it
+        visited.add(vertex)
+        print(vertex, end=' ')
 
-        # Recur for all the vertices
-        # adjacent to this vertex
-        for neighbour in self.graph[v]:
+        # Recur for all the vertices adjacent to this vertex
+        for neighbour in self.graph[vertex]:
             if neighbour not in visited:
-                self.DFSUtil(neighbour, visited)
+                self.depth_first_search_util(neighbour, visited)
 
 
-    # The function to do DFS traversal. It uses
-    # recursive DFSUtil()
-    def DFS(self, v):
+    def depth_first_search(self, vertex):
+        '''
+        The function to do DFS traversal. It uses recursive depth_first_search_util()
+        '''
 
         # Create a set to store visited vertices
         visited = set()
 
         # Call the recursive helper function
         # to print DFS traversal
-        self.DFSUtil(v, visited)
+        self.depth_first_search_util(vertex, visited)
 
 
 # Driver's code
 if __name__ == "__main__":
-    g = Graph()
-    g.addEdge(0, 1)
-    g.addEdge(0, 2)
-    g.addEdge(1, 2)
-    g.addEdge(2, 0)
-    g.addEdge(2, 3)
-    g.addEdge(3, 3)
+    graph = Graph()
+    graph.add_edge(0, 1)
+    graph.add_edge(0, 2)
+    graph.add_edge(1, 2)
+    graph.add_edge(2, 0)
+    graph.add_edge(2, 3)
+    graph.add_edge(3, 3)
 
     print("Following is Depth First Traversal (starting from vertex 2)")
 
     # Function call
-    g.DFS(2)
+    graph.depth_first_search(2)
--- 00-commonquestions-examples/edit_distance.py
+++ 00-commonquestions-examples/edit_distance.py
@@ -1,14 +1,24 @@
-def levenshteinDistance(s1, s2):
-    if len(s1) > len(s2):
-        s1, s2 = s2, s1
+def levenshtein_distance(str1, str2):
+    '''
+    Function to evaluate the differences between two strings:
+    min number of single-char edits required to change one word to another.
+    '''
+    if len(str1) > len(str2):
+        str1, str2 = str2, str1
 
-    distances = range(len(s1) + 1)
-    for i2, c2 in enumerate(s2):
-        distances_ = [i2+1]
-        for i1, c1 in enumerate(s1):
-            if c1 == c2:
-                distances_.append(distances[i1])
+    distances = range(len(str1) + 1)
+
+    for index_str2, char_str2 in enumerate(str2):
+        distances_ = [index_str2 + 1]
+        for index_str1, char_str1 in enumerate(str1):
+            if char_str1 == char_str2:
+                distances_.append(distances[index_str1])
             else:
-                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+                distances_.append(1 + min((distances[index_str1],
+                                           distances[index_str1 + 1], distances_[-1])))
         distances = distances_
+
     return distances[-1]
+
+if __name__ == "__main__":
+    print(levenshtein_distance("hippo", "happy"))
